4c3e28bb809a0d29554939105bd9ef6439746309
CodeRAFCE <seshan.io@prismscale.com>
2023-07-12 18:42:04 +0530

    sending bulk data of courier service

--- src/routes/courierCargoCostRoute.js
+++ src/routes/courierCargoCostRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { AddBulkCourierCargoTransportCost } from "../controllers/courierCargoCostController";
+import { AddBulkCourierCargoTransportCost } from "../controllers/courierCargoCostController.js";
 
 const router = express.Router();
 
