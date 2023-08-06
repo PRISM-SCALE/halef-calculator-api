2277bbd66452b891306e60c379d217a401150e65
chandan.ss <chandan.ss@prismscale.com>
2023-08-06 21:09:08 +0530

    User service bug fix

--- src/models/User.js
+++ src/models/User.js
@@ -1,26 +1,19 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 export const UserSchema = new Schema(
-	{
-		name: {type: String, required: true},
-		email: {type: String, required: true, unique: true},
-		phone: {
-			type: Number,
-			required: true,
-			unique: true,
-			// validate: {
-			// 	validator: function (v) {
-			// 		return /\d{10}/.test(v);
-			// 	},
-			// 	message: "{VALUE} is not a valid 10 digit number!",
-			// },
-		},
-		services: {type: String, required: true},
-		// countryCode: {type: String, required: true, unique: true},
-		isPhoneVerified: {type: Boolean, required: true, default: false},
-		isEmailVerified: {type: Boolean, required: true, default: false},
-	},
-	{timestamps: true}
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    service: { type: String, required: true },
+    isPhoneVerified: { type: Boolean, required: true, default: false },
+    isEmailVerified: { type: Boolean, required: true, default: false },
+  },
+  { timestamps: true }
 );
 
 const User = new mongoose.model("user", UserSchema);
