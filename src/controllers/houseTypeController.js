6b2dd44dd6313f36d102c789e35f85c1f45372f0
CodeRAFCE <seshan.io@prismscale.com>
2023-09-05 11:23:22 +0530

    reverting house types

--- src/controllers/houseTypeController.js
+++ src/controllers/houseTypeController.js
@@ -1,39 +1,45 @@
 import HouseType from "../models/houseType.js";
 
 export const getAllHouseTypes = async (req, res, next) => {
-  try {
-    const houseTypes = await HouseType.find({}).populate("allowedVehicles");
-    res.status(200).send(houseTypes);
-  } catch (error) {
-    console.error(`Error while fetching houseTypes. Details : ${error}`);
-    res
-      .status(500)
-      .send({ error: "Internal Server Error", message: "Error while fetching houseTypes. Please try again later" });
-  }
+	try {
+		const houseTypes = await HouseType.find({}).populate("allowedVehicles");
+		res.status(200).send(houseTypes);
+	} catch (error) {
+		console.error(`Error while fetching houseTypes. Details : ${error}`);
+		res.status(500).send({
+			error: "Internal Server Error",
+			message: "Error while fetching houseTypes. Please try again later",
+		});
+	}
 };
 
 export const addHouseType = async (req, res, next) => {
-  const { type, allowedVehicles } = req.body;
-  try {
-    const houseType = await HouseType.create({ type, allowedVehicles });
-    res.status(200).send(houseType);
-  } catch (error) {
-    console.error(`Error while creating houseType. Details : ${error}`);
-    res
-      .status(500)
-      .send({ error: "Internal Server Error", message: "Error while adding houseType. Please try again later" });
-  }
+	const {type, allowedVehicles, allowedRanges, isHouseTypeInDistanceRange} = req.body;
+	try {
+		const houseType = await HouseType.create({
+			type,
+			allowedVehicles,
+		});
+		res.status(200).send(houseType);
+	} catch (error) {
+		console.error(`Error while creating houseType. Details : ${error}`);
+		res.status(500).send({
+			error: "Internal Server Error",
+			message: "Error while adding houseType. Please try again later",
+		});
+	}
 };
 
 export const removeHouseType = async (req, res, next) => {
-  const { id } = req.params;
-  try {
-    const houseType = await HouseType.deleteOne({ _id: id });
-    res.status(200).send(houseType);
-  } catch (error) {
-    console.error(`Error while deleting houseType. Details : ${error}`);
-    res
-      .status(500)
-      .send({ error: "Internal Server Error", message: "Error while deleting houseType. Please try again later" });
-  }
+	const {id} = req.params;
+	try {
+		const houseType = await HouseType.deleteOne({_id: id});
+		res.status(200).send(houseType);
+	} catch (error) {
+		console.error(`Error while deleting houseType. Details : ${error}`);
+		res.status(500).send({
+			error: "Internal Server Error",
+			message: "Error while deleting houseType. Please try again later",
+		});
+	}
 };
